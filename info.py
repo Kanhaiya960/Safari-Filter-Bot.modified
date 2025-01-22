6a144f35741d392285546cbcbb6189e8698dd554
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2025-01-22 06:28:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -56,8 +56,8 @@ AUTH_REQ_CHANNEL = int(environ.get('AUTH_REQ_CHANNEL', '-1002341735077'))
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'True')), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Kanhaiya:kanhaiya960@cluster0.ljdbg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Kanhaiya")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Kanhaiya2:kanhaiya960@cluster0.mm6vo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Kanhaiya2")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Kanhaiya')
 
 #stream link shortner
