fc5817718a61884d87e1b2b2133faa6e031fba40
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2025-02-14 15:13:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Safaridevv/Safari-Filter-Bot.git /Safari-Filter-Bot
+  git clone https://github.com/Kanhaiya960/Safari-Filter-Bot.modified.git /Safari-Filter-Bot.modified
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /SuperBot
+  git clone $UPSTREAM_REPO /Safari-Filter-Bot.modified
 fi
-cd /Safari-Filter-Bot
+cd /Safari-Filter-Bot.modified
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Safari-Filter-Bot.modified...."
 python3 bot.py
