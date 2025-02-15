ef24cbc714cf6e150b143b5bfbf0cd3d20a39429
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2025-02-15 19:25:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -54,7 +54,7 @@ async def start(client, message):
             return 
         if not await db.is_user_exist(message.from_user.id):
             await db.add_user(message.from_user.id, message.from_user.first_name)
-            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention, temp.B_NAME))
+            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
         if len(message.command) != 2:
             buttons = [[
                         InlineKeyboardButton('✪ ᴜᴘᴅᴀᴛᴇꜱ ✪', callback_data='channels')
