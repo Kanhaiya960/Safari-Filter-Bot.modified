1d5a63444afd5af2d8b2d47704e58234f687461a
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2025-03-05 11:38:07 +0530

    Update telegraph.py

--- plugins/Dev_Feature/features/telegraph.py
+++ plugins/Dev_Feature/features/telegraph.py
@@ -4,41 +4,43 @@
 
 
 import os
-import requests
 import asyncio
+import requests
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
+IMGBB_API_KEY = "d4cc3d793cb68b2c6cdc2197588e895c"
 
-def upload_image_requests(image_path):
-    upload_url = "https://envs.sh"
-
+@Client.on_message(filters.command(["img", "telegraph"], prefixes="/") & filters.reply)
+async def c_upload(client, message: Message):
+    reply = message.reply_to_message
+    if not reply.media:
+        return await message.reply_text("Reply to a media to upload it to Cloud.")
+    if reply.document and reply.document.file_size > 5 * 1024 * 1024:  # 5 MB
+        return await message.reply_text("File size limit is 5 MB.")
+    msg = await message.reply_text("Processing...")
     try:
-        with open(image_path, 'rb') as file:
-            files = {'file': file} 
-            response = requests.post(upload_url, files=files)
-
-            if response.status_code == 200:
-                return response.text.strip() 
+        downloaded_media = await reply.download()
+        if not downloaded_media:
+            return await msg.edit_text("Something went wrong during download.")
+        with open(downloaded_media, "rb") as f:
+            resp = requests.post(
+                "https://api.imgbb.com/1/upload",
+                data={"key": IMGBB_API_KEY},
+                files={"image": f}
+            )
+        os.remove(downloaded_media)
+        
+        if resp.status_code == 200:
+            result = resp.json()
+            if result["success"]:
+                await msg.edit_text(f"{result['data']['url']}")
             else:
-                return print(f"Upload failed with status code {response.status_code}")
+                await msg.edit_text("Something went wrong. Please try again later.")
+        else:
+            await msg.edit_text("Something went wrong. Please try again later.")
 
     except Exception as e:
-        print(f"Error during upload: {e}")
-        return None
-
-@Client.on_message(filters.command("telegraph") & filters.private)
-async def telegraph_upload(bot, update):
-    t_msg = await bot.ask(chat_id = update.from_user.id, text = "Now Send Me Your Photo Or Video Under 5MB To Get Media Link.")
-    if not t_msg.media:
-        return await update.reply_text("**Only Media Supported.**")
-    path = await t_msg.download()
-    uploading_message = await update.reply_text("<b>ᴜᴘʟᴏᴀᴅɪɴɢ...</b>")
-    try:
-        image_url = upload_image_requests(path)
-        if not image_url:
-            return await uploading_message.edit_text("**Failed to upload file.**")
-    except Exception as error:
-        await uploading_message.edit_text(f"**Upload failed: {error}**")
+        await msg.edit_text(f"Error: {str(e)}")
         return
     await uploading_message.edit_text(
         text=f"<b>Link :-</b>\n\n<code>{image_url}</code>",
